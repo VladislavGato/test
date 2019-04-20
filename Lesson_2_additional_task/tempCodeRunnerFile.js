ec2591917c32a3c35faa6e4058ecda44ff1a2b9d
Vladislav <siriusx.info@gmail.com>
2019-04-20 23:44:29 +0600

    Закончил основное 2 задание и доп задание

--- Lesson_2_additional_task/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
- let arr = ['1835', '4229', '7392', '1513', '5555', '3123', '7639'];
-
-  for (let i = 0; i < arr.length; i++) {
-    let num = arr[i].slice(0,1);
-    if (num == 3 || num == 7) {
-      console.log( arr[i] );
-    } 
-  }
