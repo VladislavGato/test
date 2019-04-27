80c67b01d2ac1c10458bb765bb5ac25b4919f4c0
Vladislav <siriusx.info@gmail.com>
2019-04-28 02:00:03 +0600

    Закончил основное 6 задание и 6 доп и 4 доп и 5 доп

--- Lesson_5_additional_task/script.js
+++ Lesson_5_additional_task/script.js
@@ -15,6 +15,7 @@ let dataTime = {
 // let divTime = document.createElement('div');
 // document.body.appendChild(divTime);
 let body = document.querySelector('body');
+let divTime = document.getElementsByClassName('time')[0];
 // let divTime = document.createElement('div');
 // document.body.appendChild(divTime);
 
@@ -34,10 +35,10 @@ function finalDateFunc() {
     }
     return finalDateFunc;
     // console.log(finalDate);
-};
+}
 
 let finalDate = finalDateFunc();
-// console.log(finalDate);
+console.log(finalDate);
 /// совмещаем всё в 1 строку
 let dateString = `'` + finalDate.hour + ':' + finalDate.minute + ':' + 
     finalDate.second + '   '  + finalDate.dayOfMonth + '.' + finalDate.month + 
@@ -65,165 +66,37 @@ function whatDay() {
         }
     }
     return whatDay;
-};
-let dayOfTheWeek = whatDay();
-// console.log(dayOfTheWeek);
+}
 
-let divDay = document.createElement('div');
-document.body.appendChild(divDay);
+let dayOfTheWeek = whatDay();
+console.log(typeof(dayOfTheWeek) + dayOfTheWeek);
+
+let divDay = document.getElementsByClassName('day')[0];
 divDay.innerText = dayOfTheWeek;
 
 // 4) Напишите функцию, которая выводит на страницу разницу
 //  между двумя датами в количестве дней
 //-----На странице создайте интерфейс для её отображения: как минимум - 3 input’a:
 //  для двух ввода дат и вывода результата.
-let timeData = {};
-
-let divDate = document.createElement('div');
-document.body.appendChild(divDate);
-
-input11.readonly = true;
-input11.value = dsfsdvs
-
-let divInput1 = document.createElement('div');
-divDate.appendChild(divInput1);
-divInput1.innerText = '1 дата: ';
-let input1 = document.createElement('input');
-divInput1.appendChild(input1);
-
-input1.addEventListener('input', function() {
-    let items = input1.value;
-    console.log(typeof(items) + ' ' + items);
-	// timeData = items;
-    // timeData = new Date(Date.parse(items)).getFullYear();
-    timeData = new Date(Date.parse(items));
-
-    console.log(timeData);
-    
-    // yearValue.value = new Date(Date.parse(time)).getFullYear();
-    // input3.value = new Date(Date.parse(items)).setTime(milliseconds);
-    input3.value = timeData;
-    let MS_PER_SEC = 1000,
-    SEC_PER_HR = 60 * 60,
-    HR_PER_DAY = 24,
-    MS_PER_DAY = MS_PER_SEC * SEC_PER_HR * HR_PER_DAY;
-    let qwer = Math.round(timeData / MS_PER_DAY);
-    console.log(qwer  );
-
-});
-// yearValue.value = new Date(Date.parse(time)).getFullYear();
-
-let divInput2 = document.createElement('div');
-divDate.appendChild(divInput2);
-divInput2.innerText = '2 дата: ';
-let input2 = document.createElement('input');
-divInput2.appendChild(input2);
-// setTime(milliseconds)
 
 
+let text3 = document.getElementById('text3');
 
+function checkDate() {
 
+    let inputDate1 = document.getElementById('first_date').value;
+    let inputDate2 = document.getElementById('second_date').value;
 
-
-var theBigDay1 = new Date('July 1, 2015');
-var theBigDay2 = new Date('July 1, 2016');
-
-var sameAsBigDay1 = new Date();
-var sameAsBigDay2 = new Date();
-// sameAsBigDay.setTime(theBigDay.getTime());
-sameAsBigDay1.setTime(theBigDay1.getTime());
-sameAsBigDay2.setTime(theBigDay2.getTime());
-
-console.log(sameAsBigDay1);
-console.log(sameAsBigDay2);
-
-
-
-let MS_PER_SEC = 1000,
-    SEC_PER_HR = 60 * 60,
-    HR_PER_DAY = 24,
-    MS_PER_DAY = MS_PER_SEC * SEC_PER_HR * HR_PER_DAY;
-let qwer = Math.round((sameAsBigDay2.getTime() - sameAsBigDay1.getTime()) / MS_PER_DAY);
-console.log(qwer  );
-
-// startBtn.addEventListener('click', function() {
-// 	appData.startValue = true;
-// 	time = prompt("Введите дату в формате YYYY-MM-DD", '');
-// 	money = +prompt("Ваш бюджет на месяц?", '');
-
-// 	while(isNaN(money) || money == "" || money == null ) {
-// 		money = +prompt("Ваш бюджет на месяц?", '');
-// 	}
-// 	appData.budget = money;
-// 	appData.timeData = time;
-// 	budgetValue.textContent = money.toFixed();
-// 	yearValue.value = new Date(Date.parse(time)).getFullYear();
-// 	monthValue.value = new Date(Date.parse(time)).getMonth() + 1;
-// 	dayValue.value = new Date(Date.parse(time)).getDate();
-// });
-
-
-
-
-let divInput3 = document.createElement('div');
-divDate.appendChild(divInput3);
-divInput3.innerText = '3 дата: ';
-let input3 = document.createElement('input');
-divInput3.appendChild(input3);
-
-
-
-
-// appData.timeData = time;
-
-// yearValue.value = new Date(Date.parse(time)).getFullYear();
-// monthValue.value = new Date(Date.parse(time)).getMonth() + 1;
-// dayValue.value = new Date(Date.parse(time)).getDate();
-
-
-
-
-
-
-
-
-// let div3 = div.cloneNode(false);
-// let div4 = div.cloneNode(false);
-
-// let div22 = document.createElement('div');
-// document.body.appendChild(div22);
-
-// let input = document.createElement('input');
-// body.appendChild(div3);
-
-// div3.appendChild(div4);
-// div4.appendChild(input);
-
-
-// div3.appendChild(div4);
-// div4.appendChild(input);
-
-// div3.appendChild(input);
-// div3.appendChild(input);
-
-
-
-
-
-
-// var theBigDay1 = new Date('July 1, 2015');
-// var theBigDay2 = new Date('July 1, 2016');
-// var sameAsBigDay1 = new Date();
-// var sameAsBigDay2 = new Date();
-// // sameAsBigDay.setTime(theBigDay.getTime());
-// sameAsBigDay1.setTime(theBigDay1.getTime());
-// sameAsBigDay2.setTime(theBigDay2.getTime());
-// console.log(sameAsBigDay1);
-// console.log(sameAsBigDay2);
-
-// let MS_PER_SEC = 1000,
-//     SEC_PER_HR = 60 * 60,
-//     HR_PER_DAY = 24,
-//     MS_PER_DAY = MS_PER_SEC * SEC_PER_HR * HR_PER_DAY;
-// let qwer = Math.round((sameAsBigDay2.getTime() - sameAsBigDay1.getTime()) / MS_PER_DAY);
-// console.log(qwer  );
+    let date1 = new Date(inputDate1);
+    let date2 = new Date(inputDate2);
+  
+    if (date1.getTime() > date2.getTime()) {
+        let timeDiff = Math.abs(date1.getTime() - date2.getTime());
+        let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+        text3.value = diffDays;    
+    } else {
+        let timeDiff = Math.abs(date2.getTime() - date1.getTime());
+        let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+        text3.value = diffDays;
+    }
+}
